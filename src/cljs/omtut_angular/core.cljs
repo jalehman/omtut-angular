94792ede10a14b2b990ca77648aa2f01e8bae3e1
Josh Lehman <jalehman37@gmail.com>
2014-04-17 02:17:05 -0700

    better rendering!

--- src/cljs/omtut_angular/core.cljs
+++ src/cljs/omtut_angular/core.cljs
@@ -9,22 +9,33 @@
 
 (enable-console-print!)
 
+;; The app-state atom becomes our application's *cursor* when we mount the
+;; application with the call to `om/root` below. This is where your application's
+;; data should live if you want to render it into the DOM.
 (def app-state
-  (atom {:things []}))
+  (atom
+   {:phones
+    [{:name "Nexus S"
+      :snippet "Fast just got faster with Nexus S."}
+     {:name "Motorola XOOM with Wi-Fi"
+      :snippet "The Next, Next Generation tablet."}
+     {:name "MOTOROLA XOOM"
+      :snippet "The Next, Next Generation tablet."}]}))
 
-(defn omtut-angular-app [app owner]
+;; We'll destructure the cursor to get at the `phones` data
+(defn omtut-angular-app
+  [{:keys [phones]} owner]
   (reify
     om/IRender
     (render [_]
-      ;; Our HTML will always be written in CLJS. No templates!
       (html
        [:ul
-        [:li
-         [:span "Nexus S"]
-         [:p "Fast just got faster with Nexus S"]]
-        [:li
-         [:span "Motorola XOOM with Wi-Fi"]
-         [:p "The Next, Next Generation tablet."]]]))))
+        ;; There are no special "templating" constructs like `ng-repeat` needed for
+        ;; rendering elements into the DOM -- we can just use plain CLJS functions
+        ;; to manipulate our DOM elements like any other piece of data.
+        (for [phone phones]
+          [:li (:name phone)
+           [:p (:snippet phone)]])]))))
 
 (om/root omtut-angular-app app-state
          {:target (.getElementById js/document "content")})
