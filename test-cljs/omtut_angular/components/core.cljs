2ba718ecaaeae8d4e48ff8ef5cfa2ad972bd58fb
Josh Lehman <jalehman37@gmail.com>
2014-04-17 05:02:27 -0700

    cleanup

--- test-cljs/omtut_angular/components/core.cljs
+++ test-cljs/omtut_angular/components/core.cljs
@@ -22,21 +22,6 @@
                (om/root core/omtut-angular-app data {:target c})
                (count (sel c :li))))))
 
-    ;; Our "filter" uses a piece of component state called "query" to perform
-    ;; its search. Because Om components are pure functions, we can simulate any
-    ;; "query" by building it with its component state set up the way we want it.
-
-    ;; We have a new testing utility function called `wrap-component` now. In
-    ;; order to construct a component with state, we'll need a call to `om.core/build`;
-    ;; however, we need to be able to mount the component into the dummy DOM for testing
-    ;; with `om.core/root`. `wrap-component` allows us to do this. It's pretty simple.
-    ;; Pure functions for the win!
-
-    ;; Note: We could avoid this issue by putting the "query" into the cursor
-    ;; instead of in component state -- that decision is entirely up to you as the
-    ;; programmer. Either way, we should not have to write our application to be
-    ;; compatible with our testing system -- what we want is the reverse.
-
     (testing "Filters the phone list by the query in the search box"
       (is (= 3
              (let [c (common/new-container!)]
