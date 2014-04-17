7b4801460358ee3427fad4a588eea377e9c8b676
Josh Lehman <jalehman37@gmail.com>
2014-04-17 11:15:11 -0700

    finishing up

--- src/cljs/omtut_angular/core.cljs
+++ src/cljs/omtut_angular/core.cljs
@@ -1,6 +1,6 @@
 (ns omtut-angular.core
     (:require-macros [cljs.core.async.macros :refer [go alt!]])
-    (:require [cljs.core.async :refer [put! <! >! chan timeout]]
+    (:require [cljs.core.async :as async :refer [put! <! >! chan]]
               [om.core :as om :include-macros true]
               [sablono.core :as html :refer-macros [html]]
               [cljs-http.client :as http]
@@ -25,6 +25,8 @@
 ;; Next, we'll configure Google Closure HTML5 history
 (def history (Html5History.))
 
+(.setEnabled history true)
+
 (events/listen history EventType.NAVIGATE
                (fn [e]
                  (secretary/dispatch! (.-token e))))
@@ -33,6 +35,18 @@
   "Convenience function for programmatically changing route."
   [uri]
   (.setToken history uri))
+
+;; Here's our phone list route
+(defroute "/phones" []
+  (put! nav-ch [:nav {:state :phones-list}]))
+
+;; And the phone detail route
+(defroute "/phones/:phone-id" {:as params}
+  (put! nav-ch [:nav {:state :phone-view :route-params params}]))
+
+;; The catch-all route
+(defroute "*" []
+  (put! nav-ch [:nav {:state :phones-list}]))
 
 ;; ============================================================================
 ;; Router & Handlers
@@ -47,10 +61,19 @@
   [app owner [event data]]
   (case event
     :load-phones (get-phones app)
+    ;; Events of type "nav" are handled by transacting the route data into the cursor
+    :nav         (om/update! app [:location] data)
     nil))
 
 ;; ============================================================================
 ;; Sub-components
+
+;; Our stub phone-detail "view"
+(defn phone-detail
+  [phone owner]
+  (om/component
+   (html
+    [:div "TBD: detail view for " [:span (:id phone)]])))
 
 (defn phones-list
   [phones owner]
@@ -88,18 +111,20 @@
 ;; Root Component
 
 (defn omtut-angular-app
-  [{:keys [phones] :as app} owner]
+  [{:keys [phones location] :as app} owner]
   (reify
     om/IInitState
     (init-state [_]
       {:chans {:events (chan) :kill (chan)}})
     om/IWillMount
     (will-mount [_]
-      (let [{:keys [events kill]} (om/get-state owner :chans)]
+      (let [{:keys [events kill]} (om/get-state owner :chans)
+            ;; We create a new channel that ...
+            events' (async/merge [events nav-ch])]
         (go (loop []
               (alt!
-               events ([v c] (handle-event app owner v) (recur))
-               kill   ([v c] (prn "Closing event loop.")))))
+               events' ([v c] (handle-event app owner v) (recur))
+               kill    ([v c] (prn "Closing event loop.")))))
         (put! events [:load-phones nil])))
     om/IWillUnmount
     (will-unmount [_]
@@ -108,14 +133,21 @@
     (render-state [_ state]
       (html
        [:div.container
-        (om/build phones-list phones {:state state})]))))
+        (case (:state location)
+          :phones-list (om/build phones-list phones {:state state})
+          :phone-view  (om/build phone-detail
+                                 (-> (filter #(= (:id %) (get-in location [:route-params :phone-id]))
+                                             phones)
+                                     first) {:state state})
+          (change-location! "/phones"))
+        ]))))
 
 ;; ============================================================================
 ;; Om Initialization
 
 (def app-state
   (atom
-   {:phones []}))
+   {:phones [] :location {}}))
 
 (defn run! []
   (om/root omtut-angular-app app-state
